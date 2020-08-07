4f66a7ab2bfe9f38cc3d6605d0f92705087f11c4
dan-solli <dan.solli@gmail.com>
2020-08-07 10:24:07 +0200

    New component

---
+++ ottra-client/src/views/creation/components/OttraCreationFormToolbar.vue
@@ -0,0 +1,35 @@
+<template>
+
+	<v-card-title>
+		<slot>	Heading missing!  </slot>
+    
+    <v-spacer></v-spacer>
+
+    <slot name="extra_icons"></slot>
+
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn v-on="on" text icon @click="$emit('start-tour')">
+          <v-icon>help_outline</v-icon>
+        </v-btn>
+      </template>
+      {{ $t('ui.tooltip.starttour') }}
+    </v-tooltip>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn v-on="on" text icon @click="$emit('close-dialog')">
+          <v-icon>clear</v-icon>
+        </v-btn>
+      </template>
+      {{ $t('ui.text.close') }}                  
+    </v-tooltip>
+
+	</v-card-title>
+	
+</template>
+
+<script>
+export default {
+	name: "ottra-creation-form-toolbar"
+}
+</script>
